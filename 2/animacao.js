f05d524c76d0b86a877ce59202f32d44bf94d1ed
Diego Garcia <diego.ds.garcia@gmail.com>
2017-12-21 16:29:16 -0200

    First commitgit status o/

---
+++ 2/animacao.js
@@ -0,0 +1,37 @@
+function Animacao(context) {
+  this.context = context;
+  this.sprites = [];
+  this.ligado = false;
+}
+
+Animacao.prototype = {
+  novoSprite: function(sprite) {
+    this.sprites.push(sprite);
+  },
+  ligar: function() {
+    this.ligado = true;
+    this.proximoFrame();
+  },
+  desligar: function() {
+    this.ligado = false;
+  },
+  proximoFrame() {
+    if(!this.ligado) return;
+
+    this.limparTela();
+    for(var i in this.sprites) {
+      this.sprites[i].atualizar(); 
+    }
+
+    for(var i in this.sprites) {
+      this.sprites[i].desenhar();
+    }
+
+    requestAnimationFrame(() => {
+      this.proximoFrame();
+    });
+  },
+  limparTela: function() {
+    this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+  }
+}
---
+++ 3/bola.js
@@ -0,0 +1,29 @@
+function Bola(context) {
+  this.context = context;
+  this.x = 0;
+  this.y = 0;
+  this.velocidadeX = 0;
+  this.velocidadeY = 0;
+
+  this.cor = 'black';
+  this.raio = 10;
+}
+
+Bola.prototype = {
+  atualizar: function() {    
+    this.x += this.velocidadeX;
+    this.y += this.velocidadeY;
+  },
+  desenhar: function() {
+    this.context.save();
+
+    this.context.fillStyle = this.cor;
+
+    this.context.beginPath();
+    this.context.arc(this.x, this.y, this.raio, 0, Math.PI * 2, false);
+    this.context.fill();
+
+    this.context.restore();
+
+  }
+}
